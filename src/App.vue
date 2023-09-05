5cf9bb828c894e7b1998ba1d36008308305d234c
eileen-qian <yiqianc.14@gmail.com>
2023-09-05 17:15:16 +0800

    fix: app.vue 調整為 option API fix: vite.config 新增 base

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
